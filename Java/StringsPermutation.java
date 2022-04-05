3cd3860ca25e3ba9b892405fe7c06d55aefcd2af
Derrick Korku <derrickkorku1@gmail.com>
2022-04-05 16:56:05 +0000

    update

--- Java/StringsPermutation.java
+++ Java/StringsPermutation.java
@@ -11,7 +11,31 @@ public class StringsPermutation {
         return sortString(str1).equals(sortString(str2));
     }
 
-    public static String sortString(String str){
+
+    public static boolean isPermutationImproved(String str1, String str2) {
+        if (str1.length() != str2.length()) return false;
+
+        int lengths[] = new int[128];
+
+        char[] chars = str1.toCharArray();
+
+        for(char c: chars){
+            lengths[c]++;
+        }
+
+        for (int i=0; i < str2.length(); i++){
+            int c = str2.charAt(i);
+            lengths[c]--;
+
+            if (lengths[c] != 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static String sortString(String str){
         char[] content = str.toCharArray();
         java.util.Arrays.sort(content);
         return new String(content);
